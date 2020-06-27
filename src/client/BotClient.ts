b69129e732e455248d7207b234d12312c81c42b5
jRosenthal11 <pwrxpsycho@gmail.com>
2020-06-27 09:10:44 -0400

    Working on the structure

--- src/client/BotClient.ts
+++
@@ -1,3 +0,0 @@
-import { Client, User, Message } from 'discord.js';
-import { join } from 'path'
-import { prefix, owners } from '../config';
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import { Client, Message } from 'discord.js';
-import { Prefix, Token } from './Config';
+import { prefix, token } from './Config';
+import ytdl = require('ytdl-core');
 
 // login to Discord with your app's token
 const client = new Client();
+const songQueue = new Map();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.once("shardReconnecting", id => {
+    console.log(`Shard with ID ${id} reconnected`);
+});
+client.once("shardDisconnect", (event, shardID) => {
+    console.log(`Disconnected from event ${event} with ID ${shardID}`);
+});
+
+
 client.on('message', msg => {
-    if (msg.content === Prefix + 'wow') {
-        msg.reply('Pong!');
+    if (msg.author.bot) {
+        return;
+    }
+    if (!msg.content.startsWith(prefix)) {
+        return;
+    }
+    if (msg.content.startsWith(`${prefix}play`)) {
+
+    }
+    if (msg.content.startsWith(`${prefix}skip`)) {
+
+    }
+    if (msg.content.startsWith(`${prefix}stop`)) {
+        return
     }
 });
 
-client.login(Token);
+client.login(token);
